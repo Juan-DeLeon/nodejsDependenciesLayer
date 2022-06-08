4563c9b9ba69f8f7b856f348ba1782ca5bebc717
Juan De Leon <juan.deleon@definityfirst.com>
2022-06-08 16:12:47 -0500

    nodejs16.x

--- publish.sh
+++ publish.sh
@@ -11,5 +11,5 @@ aws s3 cp nodejsDependenciesLayer.zip s3://provident-lambda-dependency-layers
 aws lambda publish-layer-version \
 --layer-name nodejsDependencies \
 --description "$1" \
---compatible-runtime nodejs12.x nodejs14.x \
+--compatible-runtime nodejs12.x nodejs14.x nodejs16.x \
 --content S3Bucket=provident-lambda-dependency-layers,S3Key=nodejsDependenciesLayer.zip
